5ff2fbc42f35c1dd0e4c876ac5654c6931a174bc
YCipriani <yonatancipriani@outlook.com>
2020-02-11 13:24:43 -0500

    Initial commit (form+empty modal)

---
+++ src/app/media-form/media-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'media-form',
+  templateUrl: './media-form.component.html',
+  styleUrls: ['./media-form.component.css']
+})
+export class MediaFormComponent implements OnInit {
+  showModal;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
