149222564abb049497d12c1c197c7042fd8c6728
YCipriani <yonatancipriani@outlook.com>
2020-02-13 15:55:15 -0500

    All photos being displayed when selected + clear()

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -14,7 +14,7 @@ export class HttpService {
     //return this.http_client.get("https://api.unsplash.com/search/photos?query=london&client_id=8432caa0c84fa9846b8de16a15206f888a2c03f2e0127ea61b08107caf8eaa7a");
     //return this.http_client.get("https://api.unsplash.com/photos?client_id=8432caa0c84fa9846b8de16a15206f888a2c03f2e0127ea61b08107caf8eaa7a");
     //Requests a list of 15 random photos from the unspash API
-    return this.http_client.get("https://api.unsplash.com/photos/random?count=5&client_id=8432caa0c84fa9846b8de16a15206f888a2c03f2e0127ea61b08107caf8eaa7a");
+    return this.http_client.get("https://api.unsplash.com/photos/random?count=10&client_id=8432caa0c84fa9846b8de16a15206f888a2c03f2e0127ea61b08107caf8eaa7a");
 
   }
 }
--- src/app/media-form/media-form.component.ts
+++ src/app/media-form/media-form.component.ts
@@ -12,40 +12,129 @@ export class MediaFormComponent implements OnInit {
   name: string;
   description: string;
   element: HTMLImageElement;
+  pics: Object[];
 
   constructor(private http: HttpService) { }
 
   ngOnInit(): void {
-    var imgArray = new Array();
-    var i = 0;
-    var modal = document.getElementById("list");
     this.http.listImages().subscribe(data => {
       this.photos = data;
-      console.log(this.photos);
-      for(var pic of this.photos as Object[]) {
-        console.log("OK");
-        console.log(pic);
-        console.log(pic['urls']['raw']);
-        imgArray[i] = new Image(150,200);
-        imgArray[i].src = pic['urls']['raw'];
-        modal.appendChild(imgArray[i]);
-        imgArray[i].onclick = function(){
-          // var selected = new Image(300,350);
-          // selected.src = pic['urls']['raw'];
-          // var placement = document.getElementById("image_place");
-          // placement.appendChild(selected);
-
-          var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
-          photo_selected.src = pic['urls']['raw'];
-          var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
-          name_of_photo.value = pic['description'];
-          var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
-          desc_of_photo.value = pic['alt_description'];
-          //alert( 'You clicked me' );
-        };
-        i++;
-      }
-
+      //console.log(this.photos);
+      this.fillModal(this.photos);
     });
+  }
+
+  fillModal(photos) {
+    var imgArray = new Array();
+    var i = 0;
+    var a,b,c,d,e,f,g,h,j,k;
+    var modal = document.getElementById("list");
+    for(var pic of this.photos as Object[]) {
+      console.log(pic);
+      if (i==0) {a=pic;}
+      else if (i==1) {b=pic;}
+      else if (i==2) {c=pic;}
+      else if (i==3) {d=pic;}
+      else if (i==4) {e=pic;}
+      else if (i==5) {f=pic}
+      else if (i==6) {g=pic}
+      else if (i==7) {h=pic;}
+      else if (i==8) {j=pic;}
+      else {k=pic;}
+      imgArray[i] = new Image(150,200);
+      imgArray[i].src = pic['urls']['raw'];
+      modal.appendChild(imgArray[i]);
+      i++;
+    }
+    imgArray[0].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = a['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = a['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = a['alt_description'];
+    };
+
+    imgArray[1].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = b['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = b['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = b['alt_description'];
+    };
+    imgArray[2].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = c['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = c['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = c['alt_description'];
+    };
+    imgArray[3].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = d['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = d['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = d['alt_description'];
+    };
+    imgArray[4].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = e['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = e['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = e['alt_description'];
+    };
+    imgArray[5].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = f['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = f['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = f['alt_description'];
+    };
+    imgArray[6].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = g['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = g['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = g['alt_description'];
+    };
+    imgArray[7].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = h['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = h['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = h['alt_description'];
+    };
+    imgArray[8].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = j['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = j['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = j['alt_description'];
+    };
+    imgArray[9].onclick = function(){
+      var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+      photo_selected.src = k['urls']['raw'];
+      var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+      name_of_photo.value = k['description'];
+      var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+      desc_of_photo.value = k['alt_description'];
+    };
+    //console.log(imgArray);
+  }
+  clear() {
+    var photo_selected = document.getElementById('selected_image') as HTMLImageElement;
+    photo_selected.src = "";
+    var name_of_photo = document.getElementById('name') as HTMLTextAreaElement;
+    name_of_photo.value = "";
+    var desc_of_photo = document.getElementById('description') as HTMLTextAreaElement;
+    desc_of_photo.value = "";
   }
 }
